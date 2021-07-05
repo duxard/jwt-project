60af4963c076ae107d5312b78e1759f043f06c92
duxard <duxard@gmail.com>
2021-07-05 16:05:15 -0400

    add playground TS component

--- client-ng/src/app/app-routing.module.ts
+++ client-ng/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { PlaygroundComponent } from './components/playground/playground.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'todos', component: TodosComponent },
+  { path: 'pg', component: PlaygroundComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
@@ -20,5 +22,6 @@ export class AppRoutingModule { }
 export const routingComponents = [
   HomeComponent,
   TodosComponent,
-  PageNotFoundComponent
+  PageNotFoundComponent,
+  PlaygroundComponent
 ];
--- client-ng/src/app/app.module.ts
+++ client-ng/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {AppRoutingModule, routingComponents} from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconsModule } from './components/icons-library/icons.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
